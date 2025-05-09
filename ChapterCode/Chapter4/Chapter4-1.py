7b587160f6e7021b4493195ab7764ba86ef1b694
nibirou <741420370@qq.com>
2025-05-09 01:11:57 +0000

    更新章节4-1代码

--- ChapterCode/Chapter4/Chapter4-1.py
+++ ChapterCode/Chapter4/Chapter4-1.py
@@ -5,4 +5,18 @@
 ### 4.1.2 激活函数
 
 ### 阶跃函数sign(x)
+import numpy as np
+import matplotlib.pylab as plt
 
+def sign(x):
+    return np.array(x > 0, dtype=np.int32)
+
+def grad_sign(x):
+    return np.zeros_like(x)
+
+x = np.arange(-5.0,5.0, 0.1)
+plt.ylim(-0.1, 1.1) # 指定y轴的范围
+plt.plot(x, sign(x),label="sigmoid")
+plt.plot(x, grad_sign(x),label="derivative")
+plt.legend(loc="upper right", frameon=False)
+plt.show()
