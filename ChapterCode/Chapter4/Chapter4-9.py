1b7df5f744e7c7a624384bdfde130ffc75dd5465
nibirou <741420370@qq.com>
2025-05-12 20:09:20 +0800

    更新

--- ChapterCode/Chapter4/Chapter4-9.py
+++ ChapterCode/Chapter4/Chapter4-9.py
@@ -8,3 +8,28 @@
 
 # 多分类交叉熵损失函数
 
+import numpy as np
+
+def cross_entropy_loss_onehot(F,Y):
+    m = len(F)  #F.shape[0] 
+    return -(1./m) *np.sum(np.multiply(Y, np.log(F)))
+
+F = np.array([[0.2,0.5,0.3],[0.4,0.3,0.3]])
+Y = np.array([[0,0,1],[1,0,0]])
+cross_entropy_loss_onehot(F,Y)
+
+def cross_entropy_loss(F,Y,onehot=False):
+    m = len(F) #F.shape[0]      #样本数
+    if onehot:
+        return -(1./m) *np.sum(np.multiply(Y, np.log(F)))
+    else: return  - (1./m) *np.sum( np.log(F[range(m),Y]) )  # F[i]中对应Y[i]的那个分类的log值   
+
+F = np.array([[0.2,0.5,0.3],[0.4,0.3,0.3]])  #每行对应一个样本
+Y = np.array([2,0])  #第1个样本属于第2类、第2个样本属于第0类，主要是Y的表示方法有所不同
+
+cross_entropy_loss(F,Y)
+
+# 下面代码将整数索引数组转换为一个one-hot数组
+n_C = np.max(Y) + 1
+one_hot_y = np.eye(n_C)[Y]
+cross_entropy_loss_onehot(F, one_hot_y)
